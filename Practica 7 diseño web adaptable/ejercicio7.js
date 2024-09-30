5b9269d255ed43b57694e368000a7e3502ba7ea8
Luiartinez <luismartinez13hernandez@gmail.com>
2024-09-29 20:42:49 -0600

    Update ejercicio7.js

--- Practica 7 diseño web adaptable/ejercicio7.js
+++ Practica 7 diseño web adaptable/ejercicio7.js
@@ -1,14 +1,14 @@
 var factura = {
-    numero:650,
+    numero: 650,
     cliente: "transportes hernandez",
     vencimientos: {
-        1: new Date(2023,10,20),
-        2: new Date(2023,9,30),
-        3: new Date(2023,12,1)
+        1: new Date(2023, 10, 20),
+        2: new Date(2023, 9, 30),
+        3: new Date(2023, 12, 1)
     }
-
-}
+};
 
 var numerofactura = factura.numero;
-var primervencimiento = factura.vencimientos[3].importe;
-console.log("El segundo vencimiento de la factura "+numerofactura `sera el`+primervencimiento.getDate());
+var primervencimiento = factura.vencimientos[3]; // Solo accedemos al objeto Date
+
+console.log("El tercer vencimiento de la factura " + numerofactura + " será el " + primervencimiento.getDate());
