52b1505094a00c2fa41de3607d5f82b51e73a243
Luiartinez <luismartinez13hernandez@gmail.com>
2024-09-29 20:35:26 -0600

    Add files via upload

---
+++ ejercicio4.js
@@ -0,0 +1,15 @@
+//agregar modificar propiedades del objeto
+const libro = {
+    titulo: "EL quijote",
+    autor: "miguel de cervantes"
+  };
+  
+  //agregar una nueva propiedad
+  libro.año = 1616;
+  
+  //modificar propiedad existente
+  libro.autor="cervantes";
+  
+  //ver resultado
+  console.log(libro);
+  
